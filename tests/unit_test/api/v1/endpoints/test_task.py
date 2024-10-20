623744d9dbd1ec857bd50337406e929202c22412
Tom Chen <tom@seasalt.ai>
2024-10-20 19:55:54 +0800

    tests: refine tests

--- tests/unit_test/api/v1/endpoints/test_task.py
+++ tests/unit_test/api/v1/endpoints/test_task.py
@@ -6,54 +6,49 @@ from fastapi.testclient import TestClient
 from app.database.models.task import Task
 from app.enum.task import TaskStatus, TaskType
 from app.main import app
-from app.middleware.depends import get_db_session
+from tests.unit_test.utils import override_get_db
 
 client = TestClient(app)  # type: ignore
 
 
-@pytest.fixture
-def mock_db_session():
-    mock_session = AsyncMock()
-
-    mock_result_1 = MagicMock()
-    mock_scalars_1 = MagicMock()
-    mock_scalars_1.all.return_value = [
-        Task(
-            id="test",
-            status=TaskStatus.PENDING,
-            type=TaskType.SLEEP,
-            parameters={},
-            result={},
-            created_at="2022-01-01 00:00:00",
-            updated_at="2022-01-01 00:00:00",
-            started_at="2022-01-01 00:00:00",
-            ended_at="2022-01-01 00:00:00",
-        )
-    ]
-    mock_result_1.scalars.return_value = mock_scalars_1
-    mock_session.execute.return_value = mock_result_1
-
-    mock_result_2 = MagicMock()
-    mock_scalars_2 = MagicMock()
-    mock_scalars_2.one.return_value = 1
-    mock_result_2.scalars.return_value = mock_scalars_2
-    mock_session.execute.return_value = mock_result_2
-
-    mock_execute_side_effect = [mock_result_1, mock_result_2]
-    mock_session.execute.side_effect = mock_execute_side_effect
-    return mock_session
-
-
-@pytest.fixture
-def override_get_db(mock_db_session):
-    app.dependency_overrides[get_db_session] = lambda: mock_db_session
-    yield
-    app.dependency_overrides.clear()
-
-
 @pytest.mark.asyncio
-async def test_get_tasks(override_get_db):
-    response = client.get("/api/v1/tasks")
+async def test_get_tasks_normal_return():
+    def get_execute_side_effect():
+        mock_session = AsyncMock()
+
+        mock_result_1 = MagicMock()
+        mock_scalars_1 = MagicMock()
+        mock_scalars_1.all.return_value = [
+            Task(
+                id="test",
+                status=TaskStatus.PENDING,
+                type=TaskType.SLEEP,
+                parameters={},
+                result={},
+                created_at="2022-01-01 00:00:00",
+                updated_at="2022-01-01 00:00:00",
+                started_at="2022-01-01 00:00:00",
+                ended_at="2022-01-01 00:00:00",
+            )
+        ]
+        mock_result_1.scalars.return_value = mock_scalars_1
+        mock_session.execute.return_value = mock_result_1
+
+        mock_result_2 = MagicMock()
+        mock_scalars_2 = MagicMock()
+        mock_scalars_2.one.return_value = 1
+        mock_result_2.scalars.return_value = mock_scalars_2
+        mock_session.execute.return_value = mock_result_2
+
+        mock_execute_side_effect = [mock_result_1, mock_result_2]
+        mock_session.execute.side_effect = mock_execute_side_effect
+        return mock_session
+
+    with override_get_db(
+        app=app,
+        execute_side_effect=get_execute_side_effect(),
+    ):
+        response = client.get("/api/v1/tasks")
     assert response.status_code == 200
     response_json = response.json()
     assert response_json == {
--- tests/unit_test/utils.py
+++ tests/unit_test/utils.py
@@ -1,2 +1,14 @@
-def setup_environment():
-    pass
+from contextlib import contextmanager
+from typing import List
+from unittest.mock import MagicMock
+
+from fastapi import FastAPI
+
+from app.middleware.depends import get_db_session
+
+
+@contextmanager
+def override_get_db(app: FastAPI, execute_side_effect: List[MagicMock] = []):
+    app.dependency_overrides[get_db_session] = lambda: execute_side_effect
+    yield
+    app.dependency_overrides.clear()
